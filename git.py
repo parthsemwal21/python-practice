c6485fde23cc0b5c56603d9ad5a88809ded5363f
PARTH SEMWAL <parthsemwal317@gmail.com>
2026-09-01 10:26:19 +0530

    Add files via upload
    Mastering Nested Loops with Patterns in Python 🐍✨
    ​Nested loops aur indexing ke concepts ko strong karne ke liye pattern printing sabse best practice hai. Aaj solve kiye kuch classic problems:
    ​Square & Matrix Patterns (Rows and Columns mapping)
    ​Right-Angled Triangle (Dynamic inner loop scaling)
    ​Inverted & Mirrored Triangles (Space management & decrements)
    ​Pyramids & Diamonds (Odd star spacing logic)

---
+++ git.py
@@ -0,0 +1,232 @@
+# password authorization
+
+# password=""
+# while password !="Parth123":
+#     password=input("enter your password here ")
+
+# else:
+#     print("unlocked : now you can access")    
+
+
+# star patern code
+
+# for i in range (6):
+#     for j in range(6):
+#      print("*", end = " ")
+#     print()
+
+
+# count 1 to 100 in a matrix
+
+# rows= 10
+# num =1
+# for i in range (rows):
+#     for j in range(rows):
+#      print(num, end = "\t ")
+#      num += 1
+#     print()
+
+
+# triangle from star
+
+
+# for i in range (1,6):
+#     for j in range(1,i+1):
+#        print(j, end=" ")
+#     print()
+    
+
+
+# pattern questions 
+
+
+# for i in range (1,6):
+#     for j in range (1,6):
+#         print(j, end = " ")
+#     print()
+
+
+
+# pattern question of printing inverted triangle
+
+ 
+# for i in range (5,0,-1):
+#     for  j in range(5,0,-1):
+#         print(i, end = " ")
+#     print()  
+
+
+ # pattern question of printing same questions
+  
+
+# for i in range (1,6):
+#    for j in range(1,6):
+#         print(j, end = " ")
+#    print()  
+
+
+
+# star triangle but up to down
+
+# for i in range (1,6):
+#    for j in range(1,i+1):
+#         print("*", end = " ")
+#    print()  
+
+
+# star inverted triangle 
+
+# for i in range (5,0,-1):
+#    for j in range(1,i+1):
+#         print( "*", end = " ")
+#    print()  
+
+
+# printing triangle wwith numbers
+
+# for i in range (1,6):
+#    for j in range(1,i+1):
+#         print(j, end = " ")
+#    print() 
+
+
+# inverted triangle with numbers 
+
+
+# for i in range (1,6):
+#    for j in range(i,0,-1):
+#         print(j, end = " ")
+#    print() 
+
+
+
+# dynamic pattern questions
+
+# n=int(input("enter your numbers is:"))
+
+# for i in range (1,n+1):
+#    for j in range(i,0,-1):
+#         print(j, end = " ")
+#    print() 
+
+
+
+# reverse the numbers in triangle
+
+
+# for i in range (5,0,-1):
+#    for j in range(1,i+1):
+#         print(j, end = " ")
+#    print() 
+
+
+# revserse the triangle with 5 to 1
+
+# for i in range (1,6,):
+#    for j in range(5,i-1,-1):
+#         print(j, end = " ")
+#    print() 
+
+
+
+# revserse the triangle dynamic
+
+# n=int(input("enter your number :"))
+
+# for i in range (1,n+1):
+#    for j in range(n,i-1,-1):
+#         print(j, end = " ")
+#    print() 
+
+
+# revserse the triangle with 5 to 1
+
+# for i in range (5,0,-1):
+#    for j in range(i,0,-1):
+#         print(j, end = " ")
+#    print() 
+
+
+# revserse the triangle with 5 to 1
+
+# for i in range (1,6):
+#    for j in range(1,i+1):
+#         print(j, end = " ")
+#    print() 
+# for i in range (4,0,-1):
+#    for j in range(1,i+1):
+#         print(j, end = " ")
+#    print() 
+
+# revserse the triangle dynamic program
+
+# for i in range (5,0,-1):
+#    for j in range(5,i-1,-1):
+#         print(j, end = " ")
+#    print() 
+# for i in range (2,6):
+#    for j in range(5,i-1,-1):
+#         print(j, end = " ")
+#    print() 
+ 
+
+# space pattern
+
+# for i in range(1,6):
+#     for k in range(1,6-i):
+#         print(" " ,end = " ")
+#     for j in range(1,i+1):
+#         print(j, end =" ")
+#     print()
+
+
+# space pattern 2
+
+# for i in range(5,0,-1):
+#     for k in range(1,i-1 +1):
+#         print(" " ,end = " ")
+#     for j in range(5,i-1,-1):
+#         print(j, end =" ")
+#     print()
+
+
+
+# space diamond pyramid  pattern 3
+
+# for i in range(1,6):
+#     for j in range(1,5-i +1):
+#         print(" " , end =" ")
+#     for  k in range(1,(i*2)-1 +1):
+#         print(k,end = " ")
+#     print()
+
+# for i in range(4,0,-1):
+#      for j in range(1,5-i +1):
+#         print(" " , end =" ")
+#      for  k in range(1,(i*2)-1 +1):
+#         print(k,end = " ")
+#      print()
+
+
+
+# star wih addition sign diamond pyramid pattern 
+
+# for i in range(1,6):
+#     for j in range(1,5-i +1):
+#         print(" " , end =" ")
+#     for  k in range(1,(i*2)-1 +1):
+#         print("+", end = " ")
+#     print()
+
+# for i in range(4,0,-1):
+#      for j in range(1,5-i +1):
+#         print(" " , end =" ")
+#      for  k in range(1,(i*2)-1 +1):
+#         print("+",end = " ")
+#      print()
+
+
+
+
+
+
