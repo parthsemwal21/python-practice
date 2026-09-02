aaa40c20d59c221166a67496dea9a3fed91310db
PARTH SEMWAL <parthsemwal317@gmail.com>
2026-09-02 11:44:40 +0530

    Add files via upload

---
+++ dt.py
@@ -0,0 +1,12 @@
+name = 'sachin'
+age = 23
+gender = "male"
+is_student = True
+
+print("name",name)
+print("age",age)
+print("gender",gender)
+print(type(is_student))
+print(type(name))
+print(type(age))
+# //can check the type of variable using type() function
