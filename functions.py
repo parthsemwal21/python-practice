ef6313c07e48614e96007487d0f381de0607ff31
PARTH SEMWAL <parthsemwal317@gmail.com>
2026-09-02 11:43:09 +0530

    Add files via upload
    Starting off with the core building blocks of programming today—Functions! Focused on syntax, passing arguments, return values, and writing modular code.
    ​Pushed today's practice exercises and notes to GitHub as part of my learning journey. One commit at a time! 💻
    ​#CodingJourney #GitHub #LearnInPublic #ProgrammingBasics #DeveloperLife

---
+++ functions.py
@@ -0,0 +1,40 @@
+# first function 
+
+
+# def greet():
+#     print("Hello! Welcome to our program")
+#     print("this is my first python function program")
+# greet()
+# greet()
+
+
+# ask a number from user and print if it is even or odd using function
+
+# def odd_even():
+#    num = int((input("enter your number:")))
+#    if num%2==0:
+#             print("this is even number")
+#    else:
+#             print("this is odd number")
+# odd_even()     
+# odd_even()       
+     
+     
+
+
+# print factor of  a numbers entered by user using function
+
+# def print_factor():
+#   n = int(input("enter your number="))
+#   for i in range(1,n +1):
+#     if n % i==0:
+#          print(i, end=" ")
+# print_factor()
+# print()
+# print_factor()
+
+
+
+
+
+
