8edc3470eb2770cbfae9accfcfc03dfcaa99c756
PARTH SEMWAL <parthsemwal317@gmail.com>
2026-09-02 20:47:26 +0530

    Add files via upload

--- practiceset.py
+++ practiceset.py
@@ -1,71 +1,23 @@
-# price = 499
-# quanity = 3
-# print("total price is;",price*quanity)
+# q1. Write a function fizzbuzz(n) that takes a single number and prints "Fizz" if it's divisible by 3, "Buzz" if it's divisible by 5, "FizzBuzz"
+# 5 if it's divisible by both, otherwise print the number itself
 
+def fizbuzz(n):
+    if n%3==0 and n%5==0:
+         print("fizzbuzz")
+    elif n%3==0:
+          print("fizz")
+    elif n%5==0:
+     print("buzz")
+    return(n)
+    
 
-# monthly_salary = 35000
-# print("annualy salary is:",monthly_salary*12)
+ans=fizbuzz(15)
+ans=fizbuzz(9)
+ans=fizbuzz(20)
+ans=fizbuzz(7)
 
+print(ans)  
 
-# maths = 78
-# python = 85
-# sql = 72
-# print("total scored by student is :",maths+python+sql)
-# print("average scored by student is :",maths+python+sql/3)
-# print (" percentage scored by the student :",maths+python+sql/300*100)
-
-
-
-
-# name = "parth"
-# int_age = 23
-# float_salalry = 75.5
-# is_parth_intrested = True
-
-# print(type(name))
-# print(type(int_age))
-# print(type(float_salalry))
-# print(type(is_parth_intrested))
-
-
-# name = "t-shirt"
-# price = 1299
-# rating = 4.5
-# is_available = True
-
-
-# print(type(name))
-# print(type(price))
-# print(type(rating))
-# print(type(is_available))
-
-
-
-# salary = 40000
-# monthly_expenses = 25000
-# print ("total savings is :",salary-monthly_expenses)
-
-# c = 25
-# f=(c*9/5)+32
-# print("temperature in fahrenheit is :",f)
-
-
-# length = 15
-# width = 8 
-# print("area of rectangle:",length*width)
-# print("perimeter of rectangle:",2*(length+width))
-
-
-# current_salary = 35000
-# print("yearly salary is:",current_salary*12)
-# print("yearly_salary after savings:",current_salary*12-100000)
-
-
-
-# shirt = 1299
-# jeans = 1999
-# shoes = 2499
-# print("total shopping bill is:",shirt+jeans+shoes)
 
 
 
